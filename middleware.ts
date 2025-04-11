f8aea71307f25736adf91db922422913e5254dfa
Shin-sibainu <shincode0712@gmail.com>
2025-04-11 13:57:36 +0900

    mvp done

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/",
   "/api/articles(.*)",
+  "/api/bookmarks(.*)",
   "/api/qiita(.*)",
   "/api/zenn(.*)",
   "/api/hackernews(.*)",
